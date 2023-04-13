73d422de1bf6ea285a870635b3a4708b4679d3d9
clair <clairlee07@yahoo.com>
2023-04-12 21:55:27 -0400

    push

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -25,17 +25,31 @@ public class RockPaperScissors {
         System.out.println("Computer plays: " + computerPlay);
 
         //Determine the winner
-        boolean playerWins(String playerMove, String computerPlay) {
+        boolean playerWins (String playerMove, String computerPlay){
             if (playerMove.equals("rock")) {
                 return computerPlay.equals("scissors");
+                System.out.println("Rock crushes scissors. You win!");
+            } else {
+                System.out.println("Paper covers rock. You lose!");
             } else if (playerMove.equals("paper")) {
                 return computerPlay.equals("rock");
+                System.out.println("Paper covers rock. You win!");
             } else {
-                return computerPlay.equals("paper");
+                System.out.println("Scissors cut paper. You lose!");
+                if (playerMove.equals(computerPlay)) {
+                    System.out.println("Its a tie!");
+                } else if (playerMove.equals("scissors")) {
+                    if (computerPlay.equals("paper")) {
+                        System.out.println("Scissors cuts paper. You win!");
+                    } else {
+                        System.out.println("Rock crushes scissors. You lose!");
+                    }
+                } else {
+                    System.out.println("Invalid input. You lose!");
+                }
             }
+
         }
 
-
-
     }
-}
+
